6f8b1035b13304228b6a0c2dbbbe20e335c710e9
mishaf1203 <sduhHDFGH345>
2022-07-10 18:05:38 +0300

    1

---
+++ src/Book.java
@@ -0,0 +1,48 @@
+import java.util.Objects;
+
+public class Book {
+
+    private final String title;
+    private int year;
+    private final Author author;
+
+    public Book( String title, int year, Author author) {
+        this.title = title;
+        this.year = year;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, year, author);
+    }
+
+    @Override
+    public String toString() {
+        return title + " " + author + " " + year;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        Author author1 = new Author( "Lev", "Tolstoy");
+        Book book1 = new Book("War and peace", 312, author1);
+
+        Author author2 = new Author( "Alex", "Pushkin");
+        Book book2 = new Book("Winter Road", 213, author2);
+
+
+        System.out.println("Год издания книги " + book1.getTitle()+ " - "+ book1.getYear()+ "; Автор книги "+ book1.getAuthor().getName()+ " "+ book1.getAuthor().getSurname());
+        System.out.println("Год издания книги " + book2.getTitle()+ " - "+ book2.getYear()+ "; Автор книги "+ book2.getAuthor().getName()+ " "+ book2.getAuthor().getSurname());
+
+        book1.setYear(1933);
+        System.out.println("Год издания книги " + book1.getTitle()+ " - "+ book1.getYear()+ "; Автор книги "+ book1.getAuthor().getName()+ " "+ book1.getAuthor().getSurname());
+
+
+        System.out.println(book1);
+
+        System.out.println(book1.hashCode() == book2.hashCode());
+
+        System.out.println(book1.equals(book2));
+
+    }
+
+}
